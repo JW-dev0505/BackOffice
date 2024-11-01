5abfbf004edf5aa1613c5cf83273ff821eac1cae
JW-dev0505 <shcherbakarina6@gmail.com>
2024-11-01 11:40:45 +0800

    change firebase messaging sw

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -4,15 +4,14 @@ importScripts("https://www.gstatic.com/firebasejs/8.8.0/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/8.8.0/firebase-messaging.js");
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "your_keys",
+  authDomain: "your_keys",
+  projectId: "your_keys",
+  storageBucket: "your_keys",
+  messagingSenderId: "your_keys",
+  appId: "your_keys",
+  measurementId: "your_keys",
 };
-
 // eslint-disable-next-line no-undef
 firebase.initializeApp(firebaseConfig);
 // eslint-disable-next-line no-undef
